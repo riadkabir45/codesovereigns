66618c0258683699c86d74e68ec080d1c75de576
injaamam <injamamul937@gmail.com>
2025-01-10 14:53:44 +0600

    Organized the frontend folder for multi page site

---
+++ frontend/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+import ProductDisplay from './pages/product-display/ProductDisplay';
+ReactDOM.render(<ProductDisplay />, document.getElementById("root"));
