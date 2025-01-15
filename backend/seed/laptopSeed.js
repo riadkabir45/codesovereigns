65fbae769bfd1700de21c8300e17a4f446ba4f67
riadkabir45 <riadkabir45@gmail.com>
2025-01-16 00:50:19 +0600

    refactor: unused lst

--- backend/seed/laptopSeed.js
+++ backend/seed/laptopSeed.js
@@ -9,7 +9,6 @@ async function seedLaptops() {
     try {
         const data = await fs.promises.readFile(join(root, 'sample', 'laptops.json'), 'utf-8'); 
         const laptops = JSON.parse(data);
-        const lst = ['Processor Model', 'RAM', 'RAM Type', 'Display Resolution', 'Display Size'];
         const randomNumber = Math.random()
         const offset = (laptops.length - range - 1)*randomNumber;
         
