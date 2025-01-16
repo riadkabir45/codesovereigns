1249423a450b2281c3a771db8c0f1bc4d2e9e130
riadkabir45 <riadkabir45@gmail.com>
2025-01-16 13:00:43 +0600

    fix: multi test

--- backend/function/generalFunctions.test.js
+++ backend/function/generalFunctions.test.js
@@ -2,7 +2,15 @@ import getRandomValueExcludingKeys from "./getRandomValueExcludingKeys";
 
 test('RandomKey: get random dictionary key excluding keys',() => {
     const testDict = {'line1':'Hi','line2':'I am Luna','line3':'Nice to meet you'}
-    const key = getRandomValueExcludingKeys(testDict,['line1']);
-    expect(key).not.toBe('line1');
-    expect(Object.keys(testDict)).toContain('line1');
+    for(let i=0;i<10;i++){
+        let key = getRandomValueExcludingKeys(testDict,['line1']);
+        expect(key).not.toBe('line1');
+        expect(Object.keys(testDict)).toContain('line1');
+        key = getRandomValueExcludingKeys(testDict,['line2']);
+        expect(key).not.toBe('line2');
+        expect(Object.keys(testDict)).toContain('line2');
+        key = getRandomValueExcludingKeys(testDict,['line3']);
+        expect(key).not.toBe('line3');
+        expect(Object.keys(testDict)).toContain('line3');
+    }
 })
