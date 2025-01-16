cef4fb0e7404a2b5e79f0bf65e7cdbdf95935553
riadkabir45 <riadkabir45@gmail.com>
2025-01-16 10:13:52 +0600

    fix: random security

--- backend/config/generalFunctions.js
+++ backend/config/generalFunctions.js
@@ -3,9 +3,14 @@ import { dirname, resolve } from 'path';
 
 export function getRandomValueExcludingKeys(dict, excludedKeys) {
     const keys = Object.keys(dict).filter(key => !excludedKeys.includes(key));
-    const randomIndex = Math.floor(Math.random() * keys.length);
+  
+    if (keys.length === 0) {
+      throw new Error("No valid keys found in the dictionary.");
+    }
+  
+    const randomIndex = crypto.randomInt(0, keys.length); 
     const randomKey = keys[randomIndex];
     return randomKey;
-}
+  }
 
 export const root = resolve(dirname(fileURLToPath(import.meta.url)),'..');
