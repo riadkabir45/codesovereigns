c7ea2f4e6e8f0042a12e61fe6f58caef2e91b26c
riadkabir45 <riadkabir45@gmail.com>
2025-02-01 21:36:55 +0600

    fix: singleton db

--- backend/config/dbinit.js
+++ backend/config/dbinit.js
@@ -2,20 +2,21 @@ import { Sequelize } from "sequelize";
 import { configDotenv } from "dotenv";
 
 configDotenv();
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_URL,
-        dialect: 'postgres',
-        logging: console.log
-    }
-);
-
+let sequelizeInstance;
 
 export default async() => {
-    await sequelize.authenticate();
-    return sequelize;
+    if(!sequelizeInstance){
+        sequelizeInstance = new Sequelize(
+            process.env.DB_NAME,
+            process.env.DB_USER,
+            process.env.DB_PASS,
+            {
+                host: process.env.DB_URL,
+                dialect: 'postgres',
+                logging: console.log
+            }
+        );
+        await sequelizeInstance.authenticate();
+    }
+    return sequelizeInstance;
 };
