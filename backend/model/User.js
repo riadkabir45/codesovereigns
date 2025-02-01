a51af4b7d78c5e17c544df0664ca8e8f86f64f8e
riadkabir45 <riadkabir45@gmail.com>
2025-02-01 21:34:56 +0600

    feat: user model

---
+++ backend/model/User.js
@@ -0,0 +1,32 @@
+import { DataTypes, Model, UUIDV4 } from "sequelize";
+import sequelizer from "../config/dbinit.js";
+
+export default async () => {
+
+    class User extends Model {}
+
+    const sequelize = await sequelizer();
+
+    User.init({
+        id:{
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: UUIDV4
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },{
+        sequelize,
+        modelName: "User"
+    });
+
+    await User.sync({ force: false });
+
+    return User;
+}
