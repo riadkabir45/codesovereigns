ac0ef1a8f2c72cb4ae700351205146aaf2ffc1b0
riadkabir45 <riadkabir45@gmail.com>
2025-02-01 11:08:53 +0600

    fix: responsive footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,11 @@ function Footer() {
             <div className="p-5 flex flex-col gap-3 justify-center items-center">
                 <div>Contact: 01795019802</div>
                 <div>Email: riadkabir45@gmail.com</div>
-                <div>Copyright © 2024 ByteBit | All Rights Reserved | Bangladesh</div>
+                <div className="flex flex-col sm:flex-row gap-2 text-center">
+                    <div>Copyright © 2024 ByteBit</div><span className="hidden sm:inline">|</span>
+                    <div>All Rights Reserved</div><span className="hidden sm:inline">|</span>
+                    <div>Bangladesh</div>
+                </div>
             </div>
         </div>
     );
